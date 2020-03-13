164c5aa69d378d0d19f900e2e65aed243f747744
Andres Velasquez <andres@upstand.studio>
2020-03-13 08:11:19 -0400

    First commit.

---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <nav>NAV</nav>
+    <nuxt />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <section class="container__hero">
+      <div class="asset5"></div>
+      <div class="asset8"></div>
+      <div class="asset6"></div>
+      <div class="asset9"></div>
+      <div class="asset7"></div>
+    </section>
+    <section class="container__content">
+      <h1>Lorem, ipsum.</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam facilis maxime ducimus vero voluptatibus cum at aspernatur natus hic alias libero accusamus minima voluptates animi quas, nihil dignissimos, blanditiis laborum aut quod pariatur quisquam adipisci! Cupiditate maxime quia exercitationem, ad aspernatur nam minima eum omnis modi ullam a odio libero itaque dolorum magnam autem hic atque repellat accusantium. Nostrum magni ratione ad perspiciatis provident nisi fugit officiis rerum voluptates amet? Itaque aut facilis facere repellat nobis earum cupiditate voluptates iusto blanditiis beatae, sint exercitationem, dicta, culpa eius debitis eaque? Velit laboriosam excepturi reprehenderit sunt illo vel voluptas quisquam consequuntur fugit!</p>
+    </section>
+  </div>
+</template>
+
+<script>
+
+export default {
+}
+</script>
